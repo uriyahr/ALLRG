c4061a0c1930c96a1e81e4d7acd70092c6a44f0d
Nicolle Ayon Campos <nicolle.ac@icloud.com>
2015-07-29 16:55:22 -0700

    Merged in Riya's code for the filtering. Font is messed up though....need to fix.

--- script.js
+++ script.js
@@ -1,69 +1,52 @@
-//Create geojson object
-//var geojson;
+var filter;
+var printFood;var ourData; 
 
 
-//tabletop code that turns the file into a json object called data
+// tabletop code that turns the file into a json object called data
 document.addEventListener('DOMContentLoaded', function() {
     var URL = "1beX_EbJAX2n4wFHuLdFHEk1vlxSPdbirjovCc2ZRK8o";
-    Tabletop.init( { key: URL, callback: convertToGeoJSON, simpleSheet: true } )
+    Tabletop.init( { key: URL, callback: saveData, simpleSheet: true } )
 })
 
 //function called by the tabletop code: converts their json object to geoJSON format that we want
-function convertToGeoJSON(data) {
-    //prints out tabletop version of data
-    console.log(data);
-    //creates empty places array to save our converted objects
-//    places=[]
-//
-//    //goes through every element of "data" and converts it to the form we expected
-    for(i=0; i<data.length; i++) {
-        //creates a place object, fills it with data
+function saveData(data){
+    ourData=data; 
 
-        if(data[i]["dairy"] === "TRUE") {
-            console.log(data[i]["entreename"]);
+    // document.getElementById("food") = ourData; 
+}
+function filterData(){
+    for(var i = 0; i< ourData.length; i++)
+    {
+        if(ourData[i][filter] === "FALSE") {
+            document.write(ourData[i]["entreename"]);
+            // document.getElementById("food").innerHTML.append(printFood);
         }
-            //
-//        place = { type: 'Feature',
-//        properties: {
-//            title: data[i]["name"],
-//                    description: data[i]["description"],
-//                    'marker-color': data[i]["hexcolor"],
-//                    'marker-size': 'large',
-//                    'marker-symbol': data[i]["markersymbol"],
-//                },
-//                geometry: {
-//                    type: 'Point',
-//                    coordinates: [data[i]["long"], data[i]["lat"]]
-//                }
-//        }
-//        if((!isNaN(data[i]["lat"]) && !isNaN(data[i]["long"])) &&
-//                !(data[i]["lat"]==="" && data[i]["long"]==="") ) {
-//            //adds our place object to places
-//            places.push(place);
-//        }
     }
-//
-//    //create a geojson file from our places array
-//    geojson = { type: 'FeaturesCollection', features: places };
-//    //call function that sets up the map
-//    setupMap(geojson);
+} 
+
+function filterDairy() {
+  filter = "dairy";
 
 }
-//
-////function that sets up the map
-//function setupMap(geo) {
-//    myLayer.setGeoJSON(geo); // Adds all of the points to the map
-//    map.fitBounds(myLayer.getBounds());
-//    map.legendControl.addLegend(document.getElementById('legend').innerHTML);
-//    m.openPopup();
-//    populateListing();
-//}
-//
-//
-////old stuff (only parts you still need from old code)
-//L.mapbox.accessToken = 'pk.eyJ1IjoiamVmZnN0ZXJuIiwiYSI6IlAzRFFiN0EifQ.mNWvayrLEw9wULuq0sopyA';
-//var map = L.mapbox.map('map', 'examples.map-20v6611k')
-//  .setView([38.12367, -76.81229], 9);
-//
-//var myLayer = L.mapbox.featureLayer().addTo(map);
-//
+
+function filterEggs() {
+    filter = "eggs";
+}
+
+function filterNuts() {
+    filter = "nuts";
+}
+
+function filterSoy() {
+    filter = "soy";
+      
+}
+
+function filterWheat() {
+    filter = "wheat";
+}
+
+
+function filterFish() {
+   filter = "fish";
+}
